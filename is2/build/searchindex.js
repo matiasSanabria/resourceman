792b96b7faf38aa8f65271dba46a4cc51ea818c6
Matt <matt31sanabria@gmail.com>
2017-03-31 12:09:14 -0400

    Modificacion del script del ambiente de produccion

---
+++ is2/build/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:50,filenames:["index.txt"],objects:{},objnames:{},objtypes:{},terms:{"\u00edndic":0,busqued:0,modul:0,pagin:0},titles:["Welcome to is2&#8217;s documentation!"],titleterms:{and:0,documentation:0,indic:0,is2:0,tabl:0,welcom:0}})
