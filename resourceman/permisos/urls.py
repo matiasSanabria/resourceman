6728879ec853e67e5487603ce6017125297d3e7e
HectorDiaz_95 <hectordiaz.1995.17@gmail.com>
2017-04-14 10:30:52 -0400

    Permisos 1.1 modificacion de views y urls

--- resourceman/permisos/urls.py
+++ resourceman/permisos/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # ex: /permisos/agregar/
+    url(r'agregar/$', views.agregarPermiso, name='agregar'),
+    # ex: /permisos/editar/
+    url(r'editar/$', views.editarPermiso, name='editar'),
 ]
--- resourceman/permisos/views.py
+++ resourceman/permisos/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import RequestContext,loader
 
 
 def index(request):
-    return HttpResponse("Hola Mundo")
+    #template = loader.get_template('templates/index.html')
+    return HttpResponse("Listar permisos")#template.render())
+
+
+def editarPermiso(request):
+    return HttpResponse("Editar permisos")
+
+
+def agregarPermiso(request):
+    return HttpResponse("Agregar permisos")
