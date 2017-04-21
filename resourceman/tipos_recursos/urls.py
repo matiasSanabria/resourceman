17d23a01264df5d84f2911680085159b1808404f
matiasSanabria <matias@hooko.de>
2017-04-21 14:38:42 -0400

    Problema para mostrar formulario de edicion de tipos de recursos solucionado

--- resourceman/tipos_recursos/urls.py
+++ resourceman/tipos_recursos/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'listar/$', views.listar_tipos_recursos, name='listar'),
     url(r'crear/$', views.crear, name='crear'),
-    url(r'editar/(?P<nombre>\d+)$', views.editar, name='editar'),
-    url(r'eliminar/(?P<nombre>\d+)$', views.eliminar, name='eliminar'),
+    url(r'editar/(?P<nombre>\w+)$', views.editar, name='editar'),
+    url(r'eliminar/(?P<nombre>\w+)$', views.eliminar, name='eliminar'),
 ]
