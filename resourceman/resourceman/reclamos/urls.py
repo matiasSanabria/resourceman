3092e34c335950d34666ec7e7210d22535be3596
matiasSanabria <matias@hooko.de>
2017-05-08 08:09:44 -0400

    Ajustes varios

--- resourceman/resourceman/reclamos/urls.py
+++ resourceman/resourceman/reclamos/urls.py
@@ -1,15 +1,13 @@
-import django.conf.urls
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # ex: /permisos/
-    # url(r'^$', views.listarPermisos, name='listar'),
-    django.conf.urls.url(r'^listar/$', views.list_reclamo, name='listarReclamos'),
-    # url(r'^listar/(?P<pk>\d+)$',), views
-    # ex: /permisos/agregar/
-    django.conf.urls.url(r'^crear/$', views.crear_reclamo, name='crearReclamo'),
-    # ex: /permisos/editar/
-    # url(r'editar/(?P<pk>\d+)$', views.editarPermiso, name='editar'),
-    django.conf.urls.url(r'^ver/(?P<pk>\d+)$', views.ver_reclamo, name="verReclamo"),
-    django.conf.urls.url(r'^editar/(?P<pk>\d+)$', views.editar_reclamo, name="editarReclamo"),
+    url(r'^listar/$', views.list_reclamo, name='listarReclamos'),
+    url(r'^crear/$', views.crear_reclamo, name='crearReclamo'),
+    url(r'^ver/(?P<pk>\d+)$', views.ver_reclamo, name="verReclamo"),
+    url(r'^editar/(?P<pk>\d+)$', views.editar_reclamo, name="editarReclamo"),
+    url(r'^listar_estados/$', views.listar_estados_reclamos, name='listar_estados_reclamos'),
+    url(r'^crear_estado/$', views.crear_estado_reclamo, name='crear_estado_reclamo'),
+    url(r'^editar_estado/(?P<codigo>\w+)$', views.editar_estado_reclamo, name="editar_estado_reclamo"),
+    url(r'^eliminar_estado/(?P<codigo>\w+)$', views.eliminar_estado_reclamo, name="eliminar_estado_reclamo"),
 ]
--- resourceman/resourceman/reclamos/views.py
+++ resourceman/resourceman/reclamos/views.py
@@ -1,4 +1,4 @@
-import django.shortcuts
+from django.shortcuts import redirect, render
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -6,6 +6,102 @@ from django.core.mail import send_mail
 
 from .forms import *
 from ..tipos_recursos.models import Recurso
+
+
+@login_required
+def listar_estados_reclamos(request):
+    """
+        Permite listar los estados de los reclamos con los siguientes datos
+
+        codigo: 
+                        del estado del reclamo que se utiliza como clave primaria
+        descripcion: 
+                        descripcion del estado del reclamo
+
+        :param request: 
+        :return: el formulario para listar los estados de los reclamos
+    """
+    mensaje = 'Listar Estados'
+    messages.add_message(request, messages.INFO, mensaje)
+    lista = EstadoReclamo.objects.all()
+    return render(request, 'reclamos/listar_estados_reclamo.html', {'lista': lista})
+
+
+@login_required
+def crear_estado_reclamo(request):
+    """
+        Permite crear un estado de reclamo con los siguientes datos
+        codigo: 
+                        del estado del reclamo que se utiliza como clave primaria
+        descripcion: 
+                        descripcion del estado del reclamo
+        :param request: 
+        :return: el formulario para crear un nuevo estado de reclamo
+    """
+    if request.method == "POST":
+        estado = EstadoReclamoForm(request.POST)
+        if estado is not None:
+            if estado.is_valid():
+                estado.save()
+                messages.success(request, "Estado guardado correctamente")
+                return redirect('crear_estado')
+        else:
+            messages.error(request, "Ocurrio un error al guardar el estado. Vuelva a intentarlo")
+
+    estado = EstadoReclamoForm()
+    return render(request, 'reclamos/crear_estado_reclamo.html', {'estado': estado})
+
+
+@login_required
+def editar_estado_reclamo(request, codigo):
+    """
+        Permite editar un estado de reclamo con los siguientes datos
+        codigo: 
+                        del estado de reclamo que se utiliza como clave primaria
+        descripcion: 
+                        descripcion del estado de reclamo
+        :param request: 
+        :return:
+    """
+    mensaje = 'Modificar Estado'
+    messages.add_message(request, messages.INFO, mensaje)
+    editar = EstadoReclamo.objects.get(codigo=codigo)
+
+    if request.method == 'POST':
+        editar_form = EstadoReclamoForm(request.POST, instance=editar)
+        if editar_form.is_valid():
+            editar_form.save()
+            return redirect('listar_estados')
+        else:
+            editar_form = EstadoReclamoForm(request.POST, instance=editar)
+
+        return render(request, 'reclamos/editar_estado_reclamo.html', {
+            'editar_form': editar_form,
+            'codigo': codigo
+        })
+    else:
+        editar_form = EstadoReclamoForm(instance=editar)
+        return render(request, 'reclamos/editar_estado_reclamo.html', {
+            'editar_form': editar_form,
+            'codigo': codigo
+        })
+
+
+@login_required
+def eliminar_estado_reclamo(request, codigo):
+    """
+        Permite eliminar estado de recurso
+
+        nombre nombre: 
+                        del estado de recurso a eliminar
+        :param request: 
+        :return: el formulario para listar los estados de recursos
+    """
+    eliminar = EstadoReclamo.objects.get(codigo=codigo)
+    mensaje = "Estado de Recurso \'%s\' eliminado..\n" % eliminar
+    messages.add_message(request, messages.INFO, mensaje)
+    eliminar.delete()
+    return redirect('../listar_estados')
 
 
 @login_required
@@ -39,12 +135,12 @@ def crear_reclamo(request):
             messages.add_message(request, messages.INFO, mensaje)
 
         crear_reclamo = CrearReclamo(instance=None)
-        return django.shortcuts.render(request, 'reclamos/crear_reclamo.html', {
+        return render(request, 'reclamos/crear_reclamo.html', {
             'crear_reclamo': crear_reclamo,
         })
     else:
         crear_reclamo = CrearReclamo(instance=None)
-        return django.shortcuts.render(request, 'reclamos/crear_reclamo.html', {
+        return render(request, 'reclamos/crear_reclamo.html', {
             'crear_reclamo': crear_reclamo,
         })
 
@@ -59,7 +155,7 @@ def list_reclamo(request):
     mensaje = 'Listar Reclamos'
     messages.add_message(request, messages.INFO, mensaje)
     reclamos = Reclamo.objects.all()
-    return django.shortcuts.render(request, 'reclamos/listar_reclamos.html', {
+    return render(request, 'reclamos/listar_reclamos.html', {
         'reclamos': reclamos
     })
 
@@ -75,7 +171,7 @@ def ver_reclamo(request, pk):
     mensaje = 'Ver Reclamo'
     messages.add_message(request, messages.INFO, mensaje)
     reclamo = Reclamo.objects.get(pk=pk)
-    return django.shortcuts.render(request, 'reclamos/ver_reclamo.html', {
+    return render(request, 'reclamos/ver_reclamo.html', {
         'reclamo': reclamo,
         'pk': pk
     })
@@ -99,17 +195,17 @@ def editar_reclamo(request, pk):
         editar_form = EditarReclamo(request.POST, instance=editar)
         if editar_form.is_valid():
             editar_form.save()
-            return django.shortcuts.redirect('listarReclamos')
+            return redirect('listarReclamos')
         else:
             editar_form = EditarReclamo(request.POST, instance=editar)
 
-            return django.shortcuts.render(request, 'reclamos/editar_reclamo.html', {
+            return render(request, 'reclamos/editar_reclamo.html', {
             'editar_form': editar_form,
             'pk': pk
         })
     else:
         editar_form = EditarReclamo(instance=editar)
-        return django.shortcuts.render(request, 'reclamos/editar_reclamo.html', {
+        return render(request, 'reclamos/editar_reclamo.html', {
             'editar_form': editar_form,
             'pk': pk
         })
