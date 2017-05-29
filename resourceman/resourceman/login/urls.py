1dfb747783064952ddb16795fd09943223f20b49
willian.martinez <willian.martinez@konecta.com.py>
2017-05-29 12:49:38 -0400

    se limpia el repositorio

--- resourceman/resourceman/login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-
-urlpatterns = [
-    url(r'^login/$', views.login_view, name='login'),
-    url(r'^logout/$', views.logout_view, name="logout"),
-]
