8d2e30d7f7292447399c23f3d3dc474e33d28956
HectorDiaz_95 <hectordiaz.1995.17@gmail.com>
2017-06-03 23:14:17 -0400

    Mantenimiento modificado, sub menu agregado

--- resourceman/mantenimiento/views.py
+++ resourceman/mantenimiento/views.py
@@ -1,11 +1,13 @@
+import datetime
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, permission_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, render_to_response
 
 from mantenimiento.models import Mantenimiento
 from tipos_recursos.models import Estados, Recurso
-from mantenimiento.forms import MantenimientoForm
+from mantenimiento.forms import MantenimientoForm, MantenimientoProgramadoForm
 
+from datetime import date, timedelta
 
 @login_required
 @permission_required('mantenimiento.per_crear_mantenimiento_recurso')
@@ -24,7 +26,7 @@ def crear_mantenimiento(request):
             recurso.estado = Estados.objects.get(codigo='MAN')
             recurso.save()
 
-            mantenimiento.tipo_mantenimiento = 'COR'
+            # mantenimiento.tipo_mantenimiento = 'COR'
             mantenimiento.save()
             messages.success(request, "Mantenimiento guardado correctamente")
             return redirect('crear_mantenimiento')
@@ -97,3 +99,51 @@ def recurso_fuera_uso(request, id):
         messages.warning(request, "El mantenimiento aun no ha iniciado")
 
     return redirect('../listar')
+
+def listarMantenimientoProgramado(request):
+    """
+    Muestra la lista de recursos con fehca de mantenimieto programado al dia
+    :param request:
+    :return:
+    """
+    mensaje = 'Listar recursos en mantenimiento programado'
+    messages.add_message(request, messages.INFO, mensaje)
+    recursos = Recurso.objects.all()
+    recursos2 = []
+    if recursos:
+        for recurso in recursos:
+            if recurso.mantenimiento_programado <= datetime.date.today():
+                recursos2.append(recurso)
+    return render(request, 'mantenimiento/listar_mantenimientos_programados.html', {'recursos': recursos2})
+
+def posponer_preventivo(request, pk):
+
+    recurso = Recurso.objects.get(codigo_recurso=pk)
+    recurso.mantenimiento_programado = date.today() + timedelta(days=1)
+    recurso.save()
+    return redirect('../listar/preventivo')
+
+def crear_mantenimiento_preventivo(request, pk):
+    """
+    Crea un nuevo mantenimiento para un recurso indicado
+    :param request:
+    :return:
+    """
+    recurso = Recurso.objects.get(codigo_recurso=pk)
+    mantenimiento = MantenimientoProgramadoForm()
+
+    mantenimiento.recurso = recurso
+    mantenimiento.tipo_recurso = recurso.tipo_recurso
+    mantenimiento.save(commit=False)
+    return render(request, 'mantenimiento/crear_mantenimiento_programado.html', {
+        'mantenimiento': mantenimiento})
+
+
+def submenu_mantenimientos(request):
+    """
+    Muestra el submenu del menu de configuraciones
+    :param request:
+    :return:
+    """
+    return render_to_response("mantenimiento/menu_mantenimientos.html")
+
