48d4f0c56f9b6be5407b7af72e0059c730549949
Matt <matt31sanabria@gmail.com>
2017-06-08 03:39:13 +0000

    Cambio de lenguaje de ingles a espanhol settings.py

--- resourceman/resourceman/settings.py
+++ resourceman/resourceman/settings.py
@@ -120,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 # TIME_ZONE = 'UTC'
 TIME_ZONE = 'America/Asuncion'
